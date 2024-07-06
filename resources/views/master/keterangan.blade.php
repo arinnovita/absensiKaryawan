60e9865108558ef959914dd7ae9314bcfc6f18b9
iwakusumah <haiyahhhhh@gmail.com>
2024-07-06 08:42:17 +0700

    fix

--- resources/views/master/keterangan.blade.php
+++ resources/views/master/keterangan.blade.php
@@ -133,7 +133,7 @@
 														<span aria-hidden="true">&times;</span>
 													</button>
 												</div>
-												<form method="POST" enctype="multipart/form-data" action="{{ route('keterangan.update/{{ $d->id }}') }}">
+												<form method="POST" enctype="multipart/form-data" action="{{ route('keterangan.update') }}">
 													@csrf
 												<div class="modal-body">
 													<div class="form-group">
