94ae6e412f40d764ac81995d05eaf8c4b4b14018
iwakusumah <haiyahhhhh@gmail.com>
2024-07-08 15:10:38 +0700

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->string('email')->unique();
+            $table->string('email', 191)->unique();
             $table->string('password');            
             $table->string('role');
             $table->rememberToken();
